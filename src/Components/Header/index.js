acfebdc7150b8ed8ba99576c375412e93320c3cb
Suraj kumar <surajkumaryadavvvv@gmail.com>
2022-04-29 14:25:23 +0530

    Homepage completed

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,5 +2,13 @@ import React from "react";
 import "./index.css";
 
 export default function Header() {
-  return <header>tvMaze</header>;
+  return (
+    <header>
+      <img
+        src="https://static.tvmaze.com/images/tvm-header-logo.png"
+        alt="tvmaze"
+        className="headerLogo"
+      />
+    </header>
+  );
 }
--- src/Components/Showscontainer/index.js
+++ src/Components/Showscontainer/index.js
@@ -1,5 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 import "./index.css";
 export default function Showscontainer() {
-  return <div className="showsContainer">Showscontainer</div>;
+  const [apiData, setApiData] = useState([]);
+
+  const getApiData = async () => {
+    await axios({
+      method: "get",
+      url: `https://api.tvmaze.com/search/shows?q=all`,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((res) => {
+        setApiData(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getApiData();
+  }, []);
+
+  return (
+    <div className="showsContainer">
+      <div className="showswithDetails">
+        {apiData?.map((shows, index) => {
+          return (
+            <div key={index} className="showImgAndDetails">
+              <img src={shows.show.image.original} alt={shows.show.name} />
+              <p className="showName">
+                {shows.show.name} <span>({shows.show.language})</span>
+              </p>{" "}
+              <p className="showGenres">
+                {shows.show.genres.map((item, ind) => {
+                  return (
+                    <>
+                      {item}
+
+                      {shows.show.genres.length - 1 === ind ? "" : ","}
+                    </>
+                  );
+                })}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
