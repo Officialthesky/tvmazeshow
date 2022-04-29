e8fa6cc8075d61bb92610c2a1c8f58f0fc09ce05
Suraj kumar <surajkumaryadavvvv@gmail.com>
2022-04-29 11:35:49 +0530

    made showscontainer

---
+++ src/Components/Showscontainer/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./index.css";
+export default function Showscontainer() {
+  return <div className="showsContainer">Showscontainer</div>;
+}
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../Components/Header";
+import Showscontainer from "../Components/Showscontainer";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Showscontainer />
     </div>
   );
 }
